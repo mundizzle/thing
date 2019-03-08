b74b34db782981904584959ad3c21f1283f17aba
mundizzle <mundi.morgado@genpact.digital>
2019-03-08 13:54:52 -0800

    verify phtml fixes

--- build.mjs
+++ build.mjs
@@ -1,7 +1,7 @@
 import fs from "fs";
 import phtmlJsx from "@phtml/jsx";
 
-const jsx = `<>${fs.readFileSync("./pattern.html", "utf-8")}</>`;
+const jsx = fs.readFileSync("./pattern.html", "utf-8");
 console.log(jsx);
 phtmlJsx
   .process(jsx, {}, { data: { dir: "ltr", pClass: "foo", name: "World" } })
