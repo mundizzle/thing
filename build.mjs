5c4a766391b422e1cae4d84be54ff048e9cc9c2a
mundizzle <mundi.morgado@genpact.digital>
2019-03-08 13:50:30 -0800

    update

---
+++ build.mjs
@@ -0,0 +1,8 @@
+import fs from "fs";
+import phtmlJsx from "@phtml/jsx";
+
+const jsx = `<>${fs.readFileSync("./pattern.html", "utf-8")}</>`;
+console.log(jsx);
+phtmlJsx
+  .process(jsx, {}, { data: { dir: "ltr", pClass: "foo", name: "World" } })
+  .then(result => console.log(result.html));
